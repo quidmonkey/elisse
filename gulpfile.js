a15300fabda773d04fa87c7275f418db48c2306b
Abe Walters <quidmonkey@gmail.com>
2015-03-08 11:24:10 -0400

    Updated gulpfile. Added index.html markup.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,20 @@
 
 var browserSync = require('browser-sync');
 var gulp = require('gulp');
+var inject = require('gulp-inject');
+
+var paths = {
+    css: 'app/**/*.css',
+    jsx: 'app/**/*.jsx'
+};
+ 
+gulp.task('inject', function () {
+    var sources = gulp.src([paths.jsx, paths.css], {read: false});
+ 
+    return gulp.src('./index.html')
+        .pipe(inject(sources))
+        .pipe(gulp.dest('./src'));
+});
 
 gulp.task('browserSync', function () {
     browserSync({
@@ -11,4 +25,9 @@ gulp.task('browserSync', function () {
     });
 });
 
-gulp.task('default', ['browserSync']);
+gulp.task('watch', function () {
+    gulp.watch([paths.css, paths.jsx], 'dev');
+});
+
+gulp.task('dev', ['browserSync', 'inject']);
+gulp.task('default', ['dev', 'watch']);
