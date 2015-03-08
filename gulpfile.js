056828a8b5453062daf8676e8a1a4b4c0d72d394
Abe Walters <quidmonkey@gmail.com>
2015-03-08 16:51:38 -0400

    Fixed browser-sync auto-reload.

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,29 @@ var inject = require('gulp-inject');
 
 var paths = {
     css: 'app/**/*.css',
+    html: 'index.html',
     jsx: 'app/**/*.jsx'
 };
  
 gulp.task('inject', function () {
     var sources = gulp.src([paths.jsx, paths.css], {read: false});
  
-    return gulp.src('./index.html')
+    return gulp.src(paths.html)
         .pipe(inject(sources))
-        .pipe(gulp.dest('./src'));
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('browserSync', function () {
     browserSync({
         server: {
-            baseDir: './'
+            baseDir: '.'
         }
     });
 });
 
 gulp.task('watch', function () {
-    gulp.watch([paths.css, paths.jsx], 'dev');
+    gulp.watch([paths.css, paths.jsx], ['inject', browserSync.reload]);
+    gulp.watch(paths.html, browserSync.reload);
 });
 
 gulp.task('dev', ['browserSync', 'inject']);
