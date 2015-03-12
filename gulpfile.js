c70271d585197c9a84054ddbcb714113b826c8a9
Abe Walters <quidmonkey@gmail.com>
2015-03-12 16:02:35 -0400

    Added gulp build step.
    Compiled source now gets outputted to dist. Updated gitignore. Renamed app/ directory to src/.

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@
 
 var bowerFiles = require('main-bower-files');
 var browserSync = require('browser-sync');
+var del = require('del');
 var gulp = require('gulp');
 var inject = require('gulp-inject');
 var react = require('gulp-react');
+var runSequence = require('run-sequence');
 var stylus = require('gulp-stylus');
 
 var paths = {
     bower: 'bower_components/**/*.{css,js}',
-    css: 'app/styles/css/**/*.css',
+    css: 'dist/styles/css/',
+    dist: 'dist/',
     html: 'index.html',
-    js: 'app/**/*.js',
-    jsx: 'app/**/*.jsx',
-    styl: 'app/styles/stylus/**/*.styl'
+    js: 'dist/**/*.js',
+    jsx: 'src/**/*.jsx',
+    styl: 'src/styles/stylus/**/*.styl'
 };
 
 gulp.task('browserSync', function () {
@@ -23,10 +26,15 @@ gulp.task('browserSync', function () {
         }
     });
 });
+
+gulp.task('clean', function (done) {
+    del.sync(paths.dist);
+    done();
+});
  
 gulp.task('inject', function () {
     var bower = gulp.src(bowerFiles(), {read: false});
-    var src = gulp.src([paths.css, paths.js], {read: false});
+    var src = gulp.src([paths.css + '**/*.css', paths.js], {read: false});
 
     return gulp.src(paths.html)
         .pipe(inject(bower, {name: 'bower'}))
@@ -37,20 +45,30 @@ gulp.task('inject', function () {
 gulp.task('react', function () {
     return gulp.src(paths.jsx)
         .pipe(react())
-        .pipe(gulp.dest('app/'));
+        .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('stylus', function () {
     return gulp.src(paths.styl)
         .pipe(stylus())
-        .pipe(gulp.dest('app/styles/css'));
+        .pipe(gulp.dest(paths.css));
 });
 
 gulp.task('watch', function () {
-    gulp.watch([paths.bower, paths.jsx], ['react']);
-    gulp.watch(paths.styl, ['stylus']);
-    gulp.watch([paths.css, paths.html, paths.js], ['inject', browserSync.reload]);
+    gulp.watch(paths.jsx, ['react', 'inject', browserSync.reload]);
+    gulp.watch(paths.styl, ['stylus', 'inject', browserSync.reload]);
+    gulp.watch(paths.html, ['inject', browserSync.reload]);
 });
 
-gulp.task('dev', ['react', 'stylus', 'inject', 'browserSync']);
-gulp.task('default', ['dev', 'watch']);
+gulp.task('build', function (done) {
+    runSequence(
+        'clean',
+        'react',
+        'stylus',
+        'inject',
+        done
+    );
+});
+
+gulp.task('dev', ['build', 'browserSync', 'watch']);
+gulp.task('default', ['dev']);
